a81c8463cb471561dac6dc4a2d6f67f24d20e791
Aayush0007 <sharmaaayush2310@gmail.com>
2025-06-26 10:13:13 +0530

    Added GA4

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './styles/index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./styles/index.css";
+import ReactGA from "react-ga4";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactGA.initialize("G-X26L1R2KHL");
+
+ReactGA.send({ 
+  hitType: "pageview", 
+  page: window.location.pathname,
+});
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
